51713dde7f8722d0eb2800ff2ad1ed5384a816f8
Ge0m <geldi.omeri@gmail.com>
2018-09-26 13:56:58 -0500

    modified js and tweaked html and css
    could not get inital js function working so went with a simple one. Tweaked css and html a little and changed background image again

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-var password = document.getElementById("password");
+/* The function is not working. Have not been able to figure out the error.
+ *
+ * function validate(){
+  
+  var password = document.getElementById("password").value;
+  var confirm_password = document.getElementById("confirm_password").value;
+  
+  if (password != confirm_password) {
+    alert("Passwords Don't Match");
+    return false;
+  }
+  else{
+    alert("Welcome to the Team!");
+  }
+} */
 
-var confPassword = document.getElementById("confPassword");
+function submitForm(){
+  alert("Welcome to the Team!");
+}
